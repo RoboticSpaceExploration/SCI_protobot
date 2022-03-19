e3988802505613db63b160d41744d0aa57cf1e4c
CalianaFortin <70739998+CalianaFortin@users.noreply.github.com>
2022-03-18 20:42:56 -1000

    added motor control

--- jetsonTX_ws/keyboard_motor_input.py
+++ jetsonTX_ws/keyboard_motor_input.py
@@ -1,18 +1,28 @@
 #! /usr/bin/python3
 
-# keyboard_input.py 
-# reads the import from the keyboard
+# keyboard_motor_input.py 
+# reads the input from the keyboard
 
 from pynput import keyboard
 
+print("Initializing detecting keyboard input...")
+
+# The on_press function detects the key pressed 
+# if it is a letter output the character 
+# else it is a special character so out put the key
 def on_press(key):
     try:
         print('Alphanumeric key pressed: {0} '.format(
             key.char))
+        if key.char == ('d'):
+            print("YOU PRESSED D")
     except AttributeError:
         print('special key pressed: {0}'.format(
             key))
 
+# The on_release function detects the key released 
+# print the key released
+# if the key is equal to esc then exit 
 def on_release(key):
     print('Key released: {0}'.format(
         key))
@@ -21,6 +31,8 @@ def on_release(key):
         return False
 
 # Collect events until released
+# keyboard.Listener is a thread.
+# a thread is used to run multiple threads (tasks, function calls) at the same time.
 with keyboard.Listener(
         on_press=on_press,
         on_release=on_release) as listener:

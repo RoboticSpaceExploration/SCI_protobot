f17488b3e010c0628712569e1420fa55ef115939
CalianaFortin <70739998+CalianaFortin@users.noreply.github.com>
2022-03-21 05:47:37 -1000

    Update hello_world_publisher_example.py

---
+++ ros_ws/src/motor_p/scripts/hello_world_publisher_example.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+# The above line will ensure the interpreter used is the first one on your environments path
+# Every python file needs to start with this
+
+import queue
+import rospy                                                # import the rospy, which is a python library for ROS.
+from std_msgs.msg import String                             # import the String message from the std_msgs package 
+
+rospy.init_node('topic_publisher')                          # initiate a node called 'topic_publisher'
+pub = rospy.Publisher('phrases', String, queue_size = 10)   # create a publisher object that will publish on the "phrases" topic
+                                                            # messages of type string
+rate = rospy.Rate(2)                                        # set a publish rate od 2 hz
+msg_str = String()                                          # create a var of type string 
+msg_str = "Hello world ros tutorial"                        # initialize 'msg_str' variable
+
+while not rospy.is_shutdown():                              # create that will go until someone stops the program execution 
+
+    pub.publish(msg_str)                                    # publish the msg_string 
+    rate.sleep()                                            # make sure the publish rate maintains at 2 Hz
+
+
+# sometimes a python file is created without execution permisions, so use the following command 
+# to add execution permissions: chmod +x name_of_python_file.py
+
